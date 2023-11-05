99596cb21131abf71552c10fde67cb9801a74d9d
Mohamed Nur <mohamednur@Mohameds-MacBook-Pro-2.local>
2023-11-04 20:32:46 -0700

    Update JS

--- js/script.js
+++ js/script.js
@@ -2,5 +2,16 @@ $(document).ready(function) {
     $("form#fillout-form").submit(function (event) {
         event.preventDefault();
         const name = $("#name").val();
+        const answer1 = $("input:radio[name=question1]:checked").val();
+        const answer2 = $("input:radio[name=question2]:checked").val();
+        const answer3 = $("input:radio[name=question3]:checked").val();
+        const answer4 = $("input:radio[name=question4]:checked").val();
+        const answer5 = $("input:radio[name=question5]:checked").val();
+
+        if (name.length === 0) {
+            alert('State your full name:');
+        } else if (answer1 === "Yes" && answer2 === "Yes") {    
+          $("#suggestion1").slideDown();
+        }
     } 
 }
