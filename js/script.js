4e897dad3e9b51359f661003635b828b427faad2
Mohamed Nur <mohamednur@Mohameds-MacBook-Pro-2.local>
2023-11-05 16:52:28 -0800

    rewrite code on JS

--- js/script.js
+++ js/script.js
@@ -1,15 +1,19 @@
-$(document).ready(function() {
-    $("form#fillout-form").submit(function (event) {
-        event.preventDefault();
-        const name = $("#name").val();
-        const answer1 = $("input:radio[name=question1]:checked").val();
-        const answer2 = $("input:radio[name=question2]:checked").val();
-        const answer3 = $("input:radio[name=question3]:checked").val();
-        const answer4 = $("input:radio[name=question4]:checked").val();
-        const answer5 = $("input:radio[name=question5]:checked").val();
+function handleFormSubmit(event) {
+    event.preventDefault();
 
+        // It will gather form data
+        let name = document.getElementById("name").value;
+        let answer1 = document.querySelector("input[name=question1]:checked").value;
+        let answer2 = document.querySelector("input[name=question2]:checked").value;
+        let answer3 = document.querySelector("input[name=question3]:checked").value;
+        let answer4 = document.querySelector("input[name=question4]:checked").value;
+        let answer5 = document.querySelector("input[name=question5]:checked").value;
+        
+        // Check for empty name 
         if (name.length === 0) {
             alert('State your full name:');
+            // Check conditions and show suggestions
+
         } else if (answer1 === "Yes" && answer2 === "Yes") {    
           $("#suggestion1").slideDown();
         } else if (answer3 === "Yes" && answer4 === "Yes") {    
@@ -18,7 +22,10 @@ $(document).ready(function() {
             $("#suggestion3").slideDown();
         };
 
+        // Display user's name in suggestions 
         $(".user_name").text(name);
+
+        // Reset the form
         $(this) [0].reset();
     }); 
 });
