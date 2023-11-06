2fb0aa1d094c5825f7dc9395ab505aceeed34486
Mohamed Nur <mohamednur@Mohameds-MacBook-Pro-2.local>
2023-11-05 18:50:01 -0800

    update css and JS

--- js/script.js
+++ js/script.js
@@ -1,31 +1,32 @@
 function handleFormSubmit(event) {
     event.preventDefault();
 
-        // It will gather form data
-        let name = document.getElementById("name").value;
-        let answer1 = document.querySelector("input[name=question1]:checked").value;
-        let answer2 = document.querySelector("input[name=question2]:checked").value;
-        let answer3 = document.querySelector("input[name=question3]:checked").value;
-        let answer4 = document.querySelector("input[name=question4]:checked").value;
-        let answer5 = document.querySelector("input[name=question5]:checked").value;
+    // It will gather form data
+    let name = document.getElementById("name").value;
+    let answer1 = document.querySelector("input[name=question1]:checked").value;
+    let answer2 = document.querySelector("input[name=question2]:checked").value;
+    let answer3 = document.querySelector("input[name=question3]:checked").value;
+    let answer4 = document.querySelector("input[name=question4]:checked").value;
+    let answer5 = document.querySelector("input[name=question5]:checked").value;
 
-        // Reset suggestions section
-        document.getElementById("suggestion1").style.display = "none";
-        document.getElementById("suggestion2").style.display = "none";
-        document.getElementById("suggestion3").style.display = "none";
+    // Reset suggestions section
+    document.getElementById("suggestion1").style.display = "none";
+    document.getElementById("suggestion2").style.display = "none";
+    document.getElementById("suggestion3").style.display = "none";
 
-        } else if (answer1 === "Yes" && answer2 === "Yes") {    
-          $("#suggestion1").slideDown();
-        } else if (answer3 === "Yes" && answer4 === "Yes") {    
-            $("#suggestion2").slideDown();
-        } else if (answer5 === "Yes") {    
-            $("#suggestion3").slideDown();
-        };
+    // It will determine result and display suggestion
+    if (answer1 === "Yes" && answer2 === "Yes") {
+        document.getElementById("suggestion1").style.display = "block";
+    } else if (answer3 === "Yes" && answer4 === "Yes") {
+        document.getElementById("suggestion2").style.display = "block";
+    } else if (answer5 === "Yes") {
+        document.getElementById("suggestion3").style.display = "block";
+    }
 
-        // Display user's name in suggestions 
-        $(".user_name").text(name);
+    // Display user's name in suggestions 
+    document.querySelector(".user_name").textContent = name;
 
-        // Reset the form
-        $(this) [0].reset();
-    }); 
-});
+    // Reset the form
+    document.getElementById("fillout-form").reset();
+
+}
